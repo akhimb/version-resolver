9f3e4c6caf4cce53e1dd6c10a8a22ca1596c0cb3
DESKTOP-0S6T9MQ\akhib <akhibalakrishnan@gmail.com>
2023-08-28 21:57:25 +0530

    code updated.

--- script.js
+++ script.js
@@ -1,29 +1,90 @@
 const https = require('https');
-
-Begin();
-
-async function Begin()
-{
-    const version = await getVersion();
-    console.log("Version is : ", version);
-}
+const versionRegex = /^(20\d{2})\.(\d{1,3})\.(\d{1,3})$/;
+const versionResolverUri = 'https://my-json-server.typicode.com/typicode/demo/posts/1';
 
 exports.handler = async (event, context) => {
     try {
         // Get the API key and clientId from the event object or any other source
-        const version = await getVersion();
+        const request = event.Records[0].cf.request;
 
-        // Construct the new URL by appending the version before the 'api' part
-        const currentUrl = event.headers['Host'] + event.path;
-        const apiIndex = currentUrl.indexOf('/api');
-        const newUrl = currentUrl.slice(0, apiIndex) + '/' + version + currentUrl.slice(apiIndex);
+        // Get the API key, clientId, and acceptVersion from the request headers
+        const apiKey = request.headers['x-api-key'][0].value;
+        const clientId = request.headers['client-id'][0].value;
+        const acceptVersion = request.headers['accept-version'][0].value;
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                newUrl: newUrl,
-            }),
+        // Check if apiKey, clientId, and acceptVersion headers are missing
+        if (!apiKey || !clientId || !acceptVersion) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: 'Required headers are missing.',
+                }),
+            };
+        }
+
+        // Check if clientId and x-api-key headers are missing
+        if (!apiKey) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: 'x-api-key is missing.',
+                }),
+            };
+        }
+
+        if (!clientId) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: 'client-id is missing.',
+                }),
+            };
+        }
+
+        // Validate the accept-version using the regex
+        if (!versionRegex.test(acceptVersion)) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: 'accept-version format is invalid.',
+                }),
+            };
+        }
+
+        // Construct the headers and query parameters
+        const headers = {
+            'x-api-key': apiKey,
         };
+
+        const queryParams = `clientId=${clientId}`;
+
+        // Construct the URL for the 'version' endpoint
+        const url = versionResolverUri + `?${queryParams}`;
+
+        // Make the GET request using the 'https' module
+        const response = await new Promise((resolve, reject) => {
+            const req = https.get(url, { headers }, (res) => {
+                let data = '';
+                res.on('data', chunk => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    resolve({
+                        statusCode: res.statusCode,
+                        headers: res.headers,
+                        body: data,
+                    });
+                });
+            });
+
+            req.on('error', reject);
+        });
+
+        // Extract the version string from the response
+        const responseData = JSON.parse(response.body);
+        const version = responseData.version;
+        // Update the request URL by replacing the 'api' part with the new version
+        request.uri = request.uri.replace('/api', '/' + version + '/api');
     } catch (error) {
         console.error('Error:', error);
         return {
@@ -34,44 +95,3 @@ exports.handler = async (event, context) => {
         };
     }
 };
-
-async function getVersion() {
-    const apiKey = 'YOUR_API_KEY';
-    const clientId = 'YOUR_CLIENT_ID';
-    const versionResolverUri = 'https://my-json-server.typicode.com/akhimb/version-resolver';
-
-    // Construct the headers and query parameters
-    const headers = {
-        'x-api-key': apiKey,
-    };
-
-    const queryParams = `clientId=${clientId}`;
-
-    // Construct the URL for the 'version' endpoint
-    const url = versionResolverUri + `?${queryParams}`;
-
-    // Make the GET request using the 'https' module
-    const response = await new Promise((resolve, reject) => {
-        const req = https.get(url, { headers }, (res) => {
-            let data = '';
-            res.on('data', chunk => {
-                data += chunk;
-            });
-            res.on('end', () => {
-                resolve({
-                    statusCode: res.statusCode,
-                    headers: res.headers,
-                    body: data,
-                });
-            });
-        });
-
-        req.on('error', reject);
-    });
-
-    // Extract the version string from the response
-    const responseData = JSON.parse(response.body);
-    const version = responseData.acceptVersion;
-    return version;
-}
-
